b09858e2d1aece4f41826a717e921f1d0929cc30
CHOI TAEYOUNG <55708666+92-Theo@users.noreply.github.com>
2020-08-12 12:12:47 +0900

    Update SocketApp

---
+++ aspnetcore/socket/SocketApp/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SocketApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureKestrel(serverOptions =>
+                {
+                    // Set properties and call methods on options
+                    serverOptions.ListenLocalhost(8007, builder =>
+                    {
+                        //builder.UseConnectionHandler();
+                    });
+                })
+                .UseStartup<Startup>();
+            });
+    }
+}
