5b0fe5f824843e0ef84876354223491c6dfce89f
CHOI TAEYOUNG <55708666+92-Theo@users.noreply.github.com>
2020-08-13 15:43:52 +0900

    Update netcore-socket

---
+++ netcore/socket/SocketClientConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketClientConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SynchronousSocketClient.StartClient();
+        }
+    }
+}
